e906a111100b6dc9c42277d9b30c8185e9d7f1eb
manuelalcocer <m.alcocer1978@gmail.com>
2016-06-19 12:34:32 +0200

    EJE 4

--- 4afase/oracle/comprobacion_eje4.sql
+++ 4afase/oracle/comprobacion_eje4.sql
@@ -1,7 +1,75 @@
 
+grant scheduler_admin to fasedos;
+
+Grant succeeded.
+
+SQL> grant create job to fasedos;
+
+Grant succeeded.
+
+SQL> grant manage scheduler to fasedos;
+
+Grant succeeded.
+
+
+
+/* LA PLANA III */
+
+/* PRIMERO INSERTO UN VIENTO QUE SUPERA LA VELOCIDAD MAXIMA PARA EL DIA DE MAÑANA */
+INSERT INTO PREDICCIONES_VIENTO VALUES (TO_DATE('20 6 2016 0:0', 'DD MM YYYY HH24:MI'), 'LA PLANA III', 300, '201°1''S');
+
+/* CREO EL MONITOR PARA ESA CENTRAL (EN MI CÓDIGO LO PUSE CON UN INTERVALO DE UN MINUTO PARA LA PRUEBA)*/
+exec PC4.CrearMonitor('LA PLANA III');
+
+
+PL/SQL procedure successfully completed.
+
+/* ESPERO UN PAR DE MINUTOS Y ESTA ES LA SALIDA */
 select * from desconexiones
 where cod_aerogenerador in (
-select codigo from aerogeneradores
-where nombre_central = 'JEREZ');
+    select codigo from aerogeneradores
+    where nombre_central = 'LA PLANA III');
 
-INSERT INTO PREDICCIONES_VIENTO VALUES (TO_DATE('1 1 2020 0:0', 'DD MM YYYY HH24:MI'), 'JEREZ', 300, '201°1''S');
+select * from desconexiones
+where cod_aerogenerador in (
+    select codigo from aerogeneradores
+  4      where nombre_central = 'LA PLANA III');
+T4510000                 20-JUN-16          21-JUN-16
+T4510001                 20-JUN-16          21-JUN-16
+T4510002                 20-JUN-16          21-JUN-16
+T4510003                 20-JUN-16          21-JUN-16
+T4510004                 20-JUN-16          21-JUN-16
+T4510005                 20-JUN-16          21-JUN-16
+T4510006                 20-JUN-16          21-JUN-16
+T4510007                 20-JUN-16          21-JUN-16
+T4510008                 20-JUN-16          21-JUN-16
+T4510009                 20-JUN-16          21-JUN-16
+T4510010                 20-JUN-16          21-JUN-16
+T4510011                 20-JUN-16          21-JUN-16
+T4510012                 20-JUN-16          21-JUN-16
+T4510013                 20-JUN-16          21-JUN-16
+T4510014                 20-JUN-16          21-JUN-16
+T4510015                 20-JUN-16          21-JUN-16
+T4510016                 20-JUN-16          21-JUN-16
+T4510017                 20-JUN-16          21-JUN-16
+T4510018                 20-JUN-16          21-JUN-16
+T4510019                 20-JUN-16          21-JUN-16
+T4510020                 20-JUN-16          21-JUN-16
+T4510021                 20-JUN-16          21-JUN-16
+T4510022                 20-JUN-16          21-JUN-16
+T4510023                 20-JUN-16          21-JUN-16
+T4510024                 20-JUN-16          21-JUN-16
+T4510025                 20-JUN-16          21-JUN-16
+T4510026                 20-JUN-16          21-JUN-16
+T4510027                 20-JUN-16          21-JUN-16
+T4510028                 20-JUN-16          21-JUN-16
+T4510029                 20-JUN-16          21-JUN-16
+T4510030                 20-JUN-16          21-JUN-16
+T4510031                 20-JUN-16          21-JUN-16
+T4510032                 20-JUN-16          21-JUN-16
+T4510033                 20-JUN-16          21-JUN-16
+T4510034                 20-JUN-16          21-JUN-16
+
+35 rows selected.
+
+SQL>
--- 4afase/oracle/pc4.sql
+++ 4afase/oracle/pc4.sql
@@ -2,27 +2,39 @@ create or replace package PC4 as
 
     procedure ComprobarPrediccionesCentral (p_central centrales.nombre%TYPE default null);
 
-    procedure CrearMonitor (p_central centrales.nombre%TYPE default null);
-
+    procedure CrearMonitor (p_central centrales.nombre%TYPE);
 end PC4;
 /
 
 create or replace package body PC4 as
 
-    procedure EnviarMail
+    procedure CrearMonitor (p_central centrales.nombre%TYPE)
     is
+        v_nombre_job varchar2(16);
     begin
-        null;
-    end EnviarMail;
+        v_nombre_job := replace(p_central, ' ', '');
+        v_nombre_job := substr(v_nombre_job, 1, 16);
 
-    procedure CrearMonitor (p_central centrales.nombre%TYPE default null)
-    is
-    begin
-        if p_central is null then
-            raise_application_error(-20012, 'ERROR: Hay que poner como parámetro el nombre de una central');
-        end if;
+        dbms_scheduler.create_job (
+            job_name            =>   v_nombre_job,
+            job_type            =>  'STORED_PROCEDURE',
+            job_action          =>  'PC4.ComprobarPrediccionesCentral',
+            start_date          =>  TO_DATE(TO_CHAR(sysdate, 'DD-MM-YYYY HH24:MI'), 'DD-MM-YYYY HH24:MI'),
+            /* PARA LA PRUEBA LO PONGO EN UN MINUTO
+                repeat_interval     =>  'FREQ=MINUTELY;INTERVAL=1',
+            */
+            /* LO LANZO TODOS LOS DIAS A LAS 10 DE LA NOCHE */
+            repeat_interval     =>  'FREQ=DAILY;BYHOUR=22',
+            number_of_arguments => 1,
+            enabled             =>  FALSE);
 
-    end CrearMonitor;
+        dbms_scheduler.set_job_argument_value(
+            job_name            => v_nombre_job,
+            argument_position   => 1,
+            argument_value      => p_central);
+
+        dbms_scheduler.enable(v_nombre_job);
+    end;
 
     procedure ComprobarPrediccionesCentral (p_central centrales.nombre%TYPE default null)
     is
@@ -56,7 +68,6 @@ create or replace package body PC4 as
             while c_velocidades%found and v_desconectado = 0 loop
                 if v_velocidad.velocidad > v_aero.vel_max_viento then
                     PC3.InsertarDesconexion (v_aero.codigo, v_fecha_comprobar);
-                    EnviarMail;
                     v_desconectado := 1;
                 end if;
                 fetch c_velocidades into v_velocidad;
@@ -67,6 +78,3 @@ create or replace package body PC4 as
 
 end PC4;
 /
-
-
-exec pc4.comprobarprediccionescentral('JEREZ');
