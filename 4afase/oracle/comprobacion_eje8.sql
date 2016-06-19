f055c7da873ddb8f93bcbf9f28be94d9a28e034f
manuelalcocer <m.alcocer1978@gmail.com>
2016-06-19 09:28:24 +0200

    ej8

--- 4afase/oracle/comprobacion_eje8.sql
+++ 4afase/oracle/comprobacion_eje8.sql
@@ -3,7 +3,7 @@ where nombre_central = 'JEREZ';
 
 /* salida */
 select codigo, nombre_modelo from aerogeneradores
-  2  where nombre_central = 'JEREZ';
+where nombre_central = 'JEREZ';
 T4130000                 S88
 T4130001                 S88
 T4130002                 S88
@@ -29,15 +29,20 @@ T4130021                 S88
 T4130022                 S88
 T4130023                 S88
 T4130025                 S88
+T4130040                 S88
 
-25 rows selected.
+26 rows selected.
 
+SQL>
 SQL>
 
 /* Cambio para que tenga 2 modelos */
 update aerogeneradores
     set nombre_modelo = 'P-10'
     where codigo like 'T413000%';
+
+select codigo, nombre_modelo from aerogeneradores
+where nombre_central = 'JEREZ';
 
 /* salida */
 
@@ -47,8 +52,7 @@ update aerogeneradores
 
 10 rows updated.
 
-SQL>
-    select codigo, nombre_modelo from aerogeneradores
+select codigo, nombre_modelo from aerogeneradores
   2  where nombre_central = 'JEREZ';
 T4130000                 P-10
 T4130001                 P-10
@@ -75,8 +79,9 @@ T4130021                 S88
 T4130022                 S88
 T4130023                 S88
 T4130025                 S88
+T4130040                 S88
 
-25 rows selected.
+26 rows selected.
 
 SQL>
 
@@ -87,17 +92,20 @@ update aerogeneradores
 set nombre_modelo = 'N100'
 where codigo like 'T413001%';
 
+select codigo, nombre_modelo from aerogeneradores
+where nombre_central = 'JEREZ';
+
 /* salida */
 update aerogeneradores
 set nombre_modelo = 'N100'
-  3  where codigo like 'T413001%';
+where codigo like 'T413001%';
+
+select codigo, nombre_modelo from aerogeneradores
+where nombre_central = 'JEREZ';
 
 10 rows updated.
 
-
-select codigo, nombre_modelo from aerogeneradores
-  2  where nombre_central = 'JEREZ';
-T4130000                 P-10
+SQL> SQL>   2  T4130000          P-10
 T4130001                 P-10
 T4130002                 P-10
 T4130003                 P-10
@@ -122,8 +130,11 @@ T4130021                 S88
 T4130022                 S88
 T4130023                 S88
 T4130025                 S88
+T4130040                 S88
 
-25 rows selected.
+26 rows selected.
+
+SQL>
 
 /* N90 */
 
@@ -133,3 +144,70 @@ update aerogeneradores
 
 select codigo, nombre_modelo from aerogeneradores
 where nombre_central = 'JEREZ';
+
+/* salida */
+
+update aerogeneradores
+    set nombre_modelo = 'N90'
+    where codigo = 'T4130018';
+
+select codigo, nombre_modelo from aerogeneradores
+where nombre_central = 'JEREZ';
+Se ha sobrepasado (3) el límite de modelos por central
+update aerogeneradores
+       *
+ERROR at line 1:
+ORA-20020: No se hará la transacción para el aerogenerador: T4130018
+ORA-06512: at "FASEDOS.PC8", line 50
+ORA-06512: at "FASEDOS.CONTROLMODELOSCENTRAL", line 2
+ORA-04088: error during execution of trigger 'FASEDOS.CONTROLMODELOSCENTRAL'
+
+
+SQL> SQL>   2  T4130000          P-10
+T4130001                 P-10
+T4130002                 P-10
+T4130003                 P-10
+T4130004                 P-10
+T4130005                 P-10
+T4130006                 P-10
+T4130007                 P-10
+T4130008                 P-10
+T4130009                 P-10
+T4130010                 N100
+T4130011                 N100
+T4130012                 N100
+T4130013                 N100
+T4130014                 N100
+T4130015                 N100
+T4130016                 N100
+T4130017                 N100
+T4130018                 N100
+T4130019                 N100
+T4130020                 S88
+T4130021                 S88
+T4130022                 S88
+T4130023                 S88
+T4130025                 S88
+T4130040                 S88
+
+26 rows selected.
+
+SQL>
+
+/* inserción múltiple */
+
+insert into aerogeneradores
+    select replace(codigo, 'M53', 'M99'), 'JEREZ', 'N90'
+    from aerogeneradores
+    where codigo like 'M53%';
+
+Se ha sobrepasado (3) el límite de modelos por central
+insert into aerogeneradores
+            *
+ERROR at line 1:
+ORA-20020: No se hará la transacción para el aerogenerador: M9900000
+ORA-06512: at "FASEDOS.PC8", line 41
+ORA-06512: at "FASEDOS.CONTROLMODELOSCENTRAL", line 2
+ORA-04088: error during execution of trigger 'FASEDOS.CONTROLMODELOSCENTRAL'
+
+
--- 4afase/oracle/pc8.sql
+++ 4afase/oracle/pc8.sql
@@ -1,21 +1,5 @@
 create or replace package PC8 as
 
-    /* ESTADO PREVIO A LA TRANSACCIÓN EN AEROGENERADORES */
-    TYPE TipoRegEstadoAnterior is record
-    (
-        central     centrales.nombre%TYPE,
-        modelo      modelos_aerogeneradores.nombre%TYPE
-    );
-    TYPE TipoTablaEstadoAnterior is table of TipoRegEstadoAnterior
-        index by aerogeneradores.codigo%TYPE;
-
-    TablaEstadoAnterior TipoTablaEstadoAnterior;
-    DUPTablaEstadoAnterior TipoTablaEstadoAnterior;
-            /* FIN ESTADO PREVIO */
-
-/* ------------------------------------------------------------------ */
-
-        /* ÚLTIMA TRANSACCIÓN EN AEREOGENERADORES */
     TYPE TipoRegCantModelos is record
     (
         cantidad    number
@@ -23,151 +7,77 @@ create or replace package PC8 as
     TYPE TipoTablaCantModelos is table of TipoRegCantModelos
         index by modelos_aerogeneradores.nombre%TYPE;
 
-    TYPE TipoRegAeros is record
-    (
-        modelo      modelos_aerogeneradores.nombre%TYPE
-    );
-    TYPE TipoTablaAeros is table of TipoRegAeros
-        index by aerogeneradores.codigo%TYPE;
-
-        /* ESTA TABLA CONTIENE A LAS OTRAS DOS */
     TYPE TipoRegModelosCentrales is record
     (
-        aeros           TipoTablaAeros,
-        cantmodelos     TipoTablaCantModelos
+        modelos     TipoTablaCantModelos
     );
     TYPE TipoTablaModelosCentrales is table of TipoRegModelosCentrales
         index by centrales.nombre%TYPE;
 
-    TablaNuevaTransaccion TipoTablaModelosCentrales;
-    DUPTablaNuevaTransaccion TipoTablaModelosCentrales;
-            /* FIN ÚLTIMA TRANSACCIÓN */
+    TablaCantModelos TipoTablaModelosCentrales;
 
-/* --------------------------------------------------------------------- */
+    procedure RellenarTablaCantModelos;
 
-    procedure LimpiarTablaEstadoAnterior;
+    procedure ComprobarTransaccion (p_codigo aerogeneradores.codigo%TYPE,
+                                    p_central centrales.nombre%TYPE,
+                                    p_modelo modelos_aerogeneradores.nombre%TYPE);
 
-    procedure GuardarEstadoAnterior (p_codigo aerogeneradores.codigo%TYPE,
-                                     p_central centrales.nombre%TYPE,
-                                     p_modelo modelos_aerogeneradores.nombre%TYPE);
-
-    procedure GuardarNuevaTransaccion (p_codigo aerogeneradores.codigo%TYPE,
-                                       p_central centrales.nombre%TYPE,
-                                       p_modelo modelos_aerogeneradores.nombre%TYPE);
-
-    procedure RestaurarCentral(p_central centrales.nombre%TYPE);
-
-    procedure ContarModelosCentral;
-
-    procedure DuplicarTablas;
 end PC8;
 /
 
 create or replace package body PC8 as
 
-    procedure LimpiarTablaEstadoAnterior
+    procedure RellenarTablaCantModelos
     is
-    begin
+        cursor c_centrales is
+            select nombre from centrales;
 
-        TablaEstadoAnterior.delete;
-        TablaNuevaTransaccion.delete;
-        dbms_output.put_line('ehehe');
-    end LimpiarTablaEstadoAnterior;
+        v_nombre_central    centrales.nombre%TYPE;
 
-    procedure GuardarEstadoAnterior (p_codigo aerogeneradores.codigo%TYPE,
-                                     p_central centrales.nombre%TYPE,
-                                     p_modelo modelos_aerogeneradores.nombre%TYPE)
-    is
-    begin
-        PC8.TablaEstadoAnterior(p_codigo).central := p_central;
-        PC8.TablaEstadoAnterior(p_codigo).modelo := p_modelo;
-    end GuardarEstadoAnterior;
+        cursor c_modelos is
+            select nombre_modelo as nombre, nvl(count(*),0) as cantidad
+                from aerogeneradores
+                where nombre_central = v_nombre_central
+                group by nombre_modelo;
 
-    procedure GuardarNuevaTransaccion (p_codigo aerogeneradores.codigo%TYPE,
-                                       p_central centrales.nombre%TYPE,
-                                       p_modelo modelos_aerogeneradores.nombre%TYPE)
-    is
-    begin
-        PC8.TablaNuevaTransaccion(p_central).aeros(p_codigo).modelo := p_modelo;
-        if PC8.TablaNuevaTransaccion(p_central).cantmodelos.exists(p_modelo) = False then
-            PC8.TablaNuevaTransaccion(p_central).cantmodelos(p_modelo).cantidad := 1;
-        else
-            PC8.TablaNuevaTransaccion(p_central).cantmodelos(p_modelo).cantidad := PC8.TablaNuevaTransaccion(p_central).cantmodelos(p_modelo).cantidad + 1;
-        end if;
-    end GuardarNuevaTransaccion;
-
-    procedure RestaurarCentral(p_central centrales.nombre%TYPE)
-    is
-
-        v_codigo    aerogeneradores.codigo%TYPE;
-
-        old_central   centrales.nombre%TYPE;
-        old_modelo    modelos_aerogeneradores.nombre%TYPE;
-
-        new_central centrales.nombre%TYPE;
+        j modelos_aerogeneradores.nombre%TYPE;
 
     begin
-        /* Borra los aerogeneradores de la ultima transaccion */
-        v_codigo := DUPTablaNuevaTransaccion(p_central).aeros.first;
-        while v_codigo is not null loop
-            if DUPTablaEstadoAnterior.exists(v_codigo) then
-                old_central := DUPTablaEstadoAnterior(v_codigo).central;
-                old_modelo := DUPTablaEstadoAnterior(v_codigo).modelo;
-                new_central := p_central;
-                dbms_output.put_line('Deshaciendo cambios en: ' || v_codigo);
-                update aerogeneradores
-                    set nombre_central = old_central, nombre_modelo = old_modelo
-                    where codigo = v_codigo;
-            else
-                dbms_output.put_line('Se borra el aerogenerador: ' || v_codigo);
-                delete from aerogeneradores
-                    where codigo = v_codigo;
-            end if;
-            v_codigo := DUPTablaNuevaTransaccion(p_central).aeros.next(v_codigo);
-        end loop;
-
-    end RestaurarCentral;
-
-    procedure ContarModelosCentral
-    is
-        v_cant_centrales    number;
-        v_cant_modelos      number;
-        v_umbral_modelos    number := 3;
-        /* esta central es el nuevo nombre de central para ese codigo de aerogeneradores */
-        v_central           centrales.nombre%TYPE;
-    begin
-        v_cant_centrales := DUPTablaNuevaTransaccion.count;
-        if v_cant_centrales > 0 then
-            v_central := DUPTablaNuevaTransaccion.first;
-            while v_central is not null loop
-                select count(count(nombre_modelo)) into v_cant_modelos
-                    from aerogeneradores
-                    where nombre_central = v_central
-                    group by nombre_modelo;
-                if v_cant_modelos > v_umbral_modelos then
-                    dbms_output.put_line('Se excedió el número máximo ( ' || v_umbral_modelos ||
-                                         ' ) de modelos de aerogeneradores... Se desharán los nuevos cambios');
-                    RestaurarCentral(v_central);
-                end if;
-                v_central := DUPTablaNuevaTransaccion.next(v_central);
+        TablaCantModelos.delete;
+        for v_central in c_centrales loop
+            v_nombre_central := v_central.nombre;
+            for v_modelo in c_modelos loop
+                TablaCantModelos(v_central.nombre).modelos(v_modelo.nombre).cantidad := v_modelo.cantidad;
             end loop;
-        end if;
-    end ContarModelosCentral;
+        end loop;
+    end RellenarTablaCantModelos;
 
-    procedure DuplicarTablas
+    procedure ComprobarTransaccion (p_codigo aerogeneradores.codigo%TYPE,
+                                    p_central centrales.nombre%TYPE,
+                                    p_modelo modelos_aerogeneradores.nombre%TYPE)
     is
+        v_umbral_modelos number := 3;
+        existe_ese_modelo  boolean := False;
+        v_cantidad_modelos number := 0;
     begin
-        DUPTablaEstadoAnterior := TablaEstadoAnterior;
-        DUPTablaNuevaTransaccion := TablaNuevaTransaccion;
-    end DuplicarTablas;
-
+        existe_ese_modelo := TablaCantModelos(p_central).modelos.exists(p_modelo);
+        if not existe_ese_modelo then
+            v_cantidad_modelos := TablaCantModelos(p_central).modelos.count;
+            if v_cantidad_modelos >= v_umbral_modelos then
+                dbms_output.put_line('Se ha sobrepasado (' || v_umbral_modelos || ') el límite de modelos por central');
+                raise_application_error(-20020, 'No se hará la transacción para el aerogenerador: ' || p_codigo);
+            end if;
+        else
+            TablaCantModelos(p_central).modelos(p_modelo).cantidad := NVL(TablaCantModelos(p_central).modelos(p_modelo).cantidad, 0) + 1;
+        end if;
+    end ComprobarTransaccion;
 end PC8;
 /
 
 create or replace trigger PrepararControlModelos
 before insert or update on aerogeneradores
 begin
-    PC8.LimpiarTablaEstadoAnterior;
+    PC8.RellenarTablaCantModelos;
 end PrepararControlModelos;
 /
 
@@ -175,66 +85,6 @@ create or replace trigger ControlModelosCentral
 before insert or update on aerogeneradores
 for each row
 begin
-    case
-        when inserting then
-            PC8.GuardarNuevaTransaccion(:new.codigo,:new.nombre_central,:new.nombre_modelo);
-        when updating then
-            PC8.GuardarEstadoAnterior(:old.codigo,:old.nombre_central,:old.nombre_modelo);
-            PC8.GuardarNuevaTransaccion(:new.codigo,:new.nombre_central,:new.nombre_modelo);
-    end case;
+    PC8.ComprobarTransaccion(:new.codigo,:new.nombre_central,:new.nombre_modelo);
 end ControlModelosCentral;
 /
-
-
-
-create or replace trigger ComprobarTablaModelos
-after insert or update on aerogeneradores
-begin
-    PC8.DuplicarTablas;
-    PC8.ContarModelosCentral;
-
-end ComprobarTablaModelos;
-/
-
-
-update aerogeneradores
-    set nombre_modelo = 'S88'
-    where codigo LIKE 'T41300%';
-
-
-select codigo, nombre_modelo from aerogeneradores
-where nombre_central = 'JEREZ';
-
-
-update aerogeneradores
-    set nombre_modelo = 'P-10'
-    where codigo like 'T413000%';
-
-update aerogeneradores
-    set nombre_modelo = 'N100'
-where codigo like 'T413001%';
-
-update aerogeneradores
-    set nombre_modelo = 'N90'
-    where codigo = 'T4130018';
-
-update aerogeneradores
-    set nombre_central = 'JEREZ'
-    where nombre_central = 'LA DEHESICA';
-
-select count(count(nombre_modelo))
-    from aerogeneradores
-    where nombre_central = 'JEREZ'
-    group by nombre_modelo;
-
-update aerogeneradores
-    set nombre_modelo = 'S88'
-    where codigo LIKE 'T41300%';
-
-insert into aerogeneradores values ('T4130040', 'JEREZ', 'N90');
-
-insert into aerogeneradores
-select replace(codigo, 'T4', 'T8'), 'JEREZ', nombre_modelo
-from aerogeneradores
-where nombre_central = 'LA DEHESICA';
-*/
